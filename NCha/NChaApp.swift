ef75c807b807268e43139a096c6b04fe0c6fcd40
Sumitgupta54856171 <sg54856171@gmail.com>
2026-03-22 17:14:18 +0530

    first commit

---
+++ NCha/NChaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NChaApp.swift
+//  NCha
+//
+//  Created by sumit on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NChaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
